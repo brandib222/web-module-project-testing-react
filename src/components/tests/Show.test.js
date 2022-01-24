8039e5915f767ad353d043f9ce96e1af6ab93aaf
brandib222 <brandi.danielle.ball@gmail.com>
2022-01-24 16:32:44 -0500

    fixed some things

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -13,7 +13,7 @@ const testShow = {
             id: 0, 
             name: 'Season 1',
             episode: []
-        }
+        },
         {
             id: 1, 
             name: 'Season 2',
@@ -23,7 +23,7 @@ const testShow = {
 };
 
 test('renders without errors', ()=>{
-    render(<Show show = {testShow} selectedSeason={'tester'}/>);
+    render(<Show show = {testShow} selectedSeason={'none'}/>);
 });
 
 test('renders Loading component when prop show is null', () => {
@@ -33,7 +33,7 @@ test('renders Loading component when prop show is null', () => {
 });
 
 test('renders same number of options seasons are passed in', ()=>{
-    render(<Show show={testShow} selectedSeason={'tester'}/>);
+    render(<Show show={testShow} selectedSeason={'none'}/>);
     const seasonOptions = screen.queryAllByTestId('season-option');
     expect(seasonOptions).toHaveLength(2);
 });
@@ -41,7 +41,7 @@ test('renders same number of options seasons are passed in', ()=>{
 test('handleSelect is called when an season is selected', () => {
     const handleSelect = jest.fn();
     render(<Show show={testShow} selectedSeason={'none'} handleSelect={handleSelect}/>);
-    const select = screen.getAllByLabelText('/Select A Season/');
+    const select = screen.getByLabelText(/Select A Season/i);
     userEvent.selectOptions(select, ['1']);
 
     expect(handleSelect).toBeCalled();
@@ -51,4 +51,9 @@ test('component renders when no seasons are selected and when rerenders with a s
     const { rerender } = render(<Show show={testShow} selectedSeason={'none'}/>);
     let episodes = screen.queryByTestId('episodes-container');
     expect(episodes).not.toBeInTheDocument();
+
+    rerender(<Show show={testShow} selectedSeason={1}/>);
+     episodes = screen.queryByTestId('episodes-container');
+     expect(episodes).toBeInTheDocument();
+
 });
